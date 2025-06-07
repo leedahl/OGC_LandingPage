ae664185b836e08956c25c1355a10ec36d7695c5
Michael Leedahl <7659158+leedahl@users.noreply.github.com>
2025-06-07 15:03:23 -0600

    Modified the services to use the key_alias specified in an environment variable.

--- src/ogc_landing/user_management/user_management_lambda.py
+++ src/ogc_landing/user_management/user_management_lambda.py
@@ -7,7 +7,6 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-import boto3
 from ogc_landing.security import user_exists, update_user_password
 
 
